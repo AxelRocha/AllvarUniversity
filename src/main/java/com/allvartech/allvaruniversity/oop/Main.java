0444d4ddab0e568030d4ceaa484fbc156d4d73f8
AxelRocha <axel.rocha@brxretail.com>
2025-07-23 16:15:44 -0300

    Tarefas de classes na main

--- src/main/java/com/allvartech/allvaruniversity/oop/Main.java
+++ src/main/java/com/allvartech/allvaruniversity/oop/Main.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Tarefas:
+            Criar branch para cada um
+            Criar classes
+            Criar, pelo menos 5, atributo nas classes de cada tipo disponivel no java (int, double, char, long, etc)
+            Criar getter e setter destes atributos
+            Criar metodos que altera esses atributos para valores aleatorios
+            Chamar as classes e atritubos na MAIN
+            Commitar as tarefas
+         */
     }
 }
